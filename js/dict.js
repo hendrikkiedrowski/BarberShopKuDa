38c0eb7dd137e18efd12acc049a3d66ff6aec527
Henrik von Kiedrowski <hkiedrowski@inet.tu-berlin.de>
2017-11-15 19:45:10 +0100

    Added AutoLanguage select and translate

---
+++ js/dict.js
@@ -0,0 +1,47 @@
+var dict = {
+    "Termin Buchen": {
+        en: "Book Now"
+    },
+    "Unser ethnisch gemischter Barbier aus Daenemark": {
+        en: "Our ethnically mixed barber from Denmark"
+    },
+    "Mit einer langjaehrigen Berufserfahrung": {
+        en: "with a big work experience"
+    },
+    'Ein "Old-School" inspirierter Perfektionist': {
+        en: "An 'old-school' inspired perfectionist"
+    },
+    "Mit einem kreativen Arbeitsstil": {
+        en: "with a creative workflow"
+    },
+    "Unser Barbier aus Litauen": {
+        en: "Our Barber from Latvia"
+    },
+    "Ein wahrer Perfektionist": {
+        en: "A true perfectionist"
+    },
+    "Saubere und praezise Schnitt-Technik": {
+        en: "clean and precise cutting technique"
+    },
+    "Studierte die Barbier-Kunst in London": {
+        en: "studied barbering in London"
+    },
+    "Unser gewandter Barbier aus England": {
+        en: "Our adroit barber from England"
+    },
+    "Fade-Spezialist für alle Haartypen": {
+        en: "Fade specialist in all hair types"
+    },
+    "Ein verfechter der Praezision": {
+        en: "A stickler for accuracy"
+    }
+};
+var translator;
+
+function translate() {
+    if ((navigator.language || navigator.userLanguage) != "de") {
+
+        translator = $('body').translate({lang: "en", t: dict}); //use English
+    }
+
+}
